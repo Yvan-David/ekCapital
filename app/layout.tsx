beeec52be88322f6c89eed02e63e4f8589be56ec
Yvan-David <irankundayvan2020@gmil.com>
2026-04-03 14:00:25 +0200

    adding a join training modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LayoutClient } from "@/sections/LayoutClient";
+import { Toaster } from 'react-hot-toast';
 
 export const metadata: Metadata = {
   title: "EK CAPITAL GROUP",
@@ -16,7 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <LayoutClient>{children}</LayoutClient>
+        <LayoutClient>
+          {children}
+          <Toaster position="top-right" reverseOrder={false} />
+        </LayoutClient>
       </body>
     </html>
   );
